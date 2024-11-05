2e120b643145d1299607efbd0b2944c29462377d
il-04 <147924370+il-04@users.noreply.github.com>
2024-11-05 01:05:27 -0800

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -1,1 +1,28 @@
+import pandas as pd
+import rumpy as np
+import matplotlib.pyplot as plt
+data = pd.read_csv('')
+data.head(10)
 
+plt.scatter(data['year'],data['own'])
+plt.title('scatter plot')
+plt.xlabel('year')
+plt.ylabel('own')
+plt.show()
+
+plt.plot(data['year'])
+plt.plot(data['own'])
+plt.title('line chart')
+plt.xlabel('year')
+plt.ylabel('own')
+plt.show()
+
+plt.bar(data['year'], data['own'])
+plt.title('bar plot')
+plt.xlabel('year')
+plt.ylabel('own')
+plt.show()
+
+plt.hist(data['income'])
+plt.title('histogram')
+plt.show()
