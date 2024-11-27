daf5186045dfba6006cea4126fdaf2db50f5dcea
il-04 <147924370+il-04@users.noreply.github.com>
2024-11-27 20:18:52 +0530

    Update task 5.py

--- task 5.py
+++ task 5.py
@@ -20,6 +20,7 @@ print(df.info())
 print()
 
 df.hist(bins = 50, grid = False, figsize = (20,15));
+plt.savefig('1.png')  
 
 print(df.describe())
 print()
@@ -36,39 +37,47 @@ df.target.value_counts().plot(kind = 'bar',color = ["orchid","salmon"])
 plt.title("Heart disease values")
 plt.xlabel("1 = Heart Disese, 0 = No Heart disease")
 plt.ylabel("Amount");
+plt.savefig('2.png')  
 
 df.target.value_counts().plot(kind = 'pie', figsize = (8,6))
 plt.legend(["Disease","No Disease"]);
+plt.savefig('3.png')  
 
 print(df.sex.value_counts())
 print()
 df.sex.value_counts().plot().plot(kind = 'pie', figsize =(8,6))
 plt.title("Male Female ratio")
 plt.legend(['Male','Female']);
+plt.savefig('4.png')  
 
 print(pd.crosstab(df.target, df.sex))
 print()
 sns.countplot(x = 'target', data = df, hue = 'sex')
 plt.title("Heart Disease frequency for sex")
 plt.xlabel("1 = Heart Disese, 0 = No Heart disease");
+plt.savefig('5.png')  
 
 print(df.cp.value_counts())
 print()
 df.cp.value_counts().plot(kind = 'bar', color = ['salmon','lightskyblue','springgreen','khaki'])
 plt.title("Chest pain type vs count")
+plt.savefig('6.png')  
 
 print(pd.crosstab(df.sex,df.cp))
 print()
 pd.crosstab(df.sex,df.cp).plot(kind = 'bar', color = ['coral','lightskyblue','plum','khaki'])
 plt.xlabel("1 = Male, 0 = Female");
+plt.savefig('7.png')  
 
 print(pd.crosstab(df.cp,df.target))
 print()
 
 sns.countplot(x = 'cp', data = df, hue = 'target');
+plt.savefig('8.png')  
 
 sns.displot(x='age', data =df, bins =30, kde = True);
+plt.savefig('9.png')  
 
 sns.displot(x='thalach', data =df, bins =30, kde = True, color = 'chocolate');
-
+plt.savefig('10.png')  
 
