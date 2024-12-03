4a3575e18d9cbf328dafa5ab6147e534f03c13bc
il-04 <147924370+il-04@users.noreply.github.com>
2024-12-03 10:47:55 +0530

    Update task 6.py

--- task 6.py
+++ task 6.py
@@ -1,1 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('Movies.csv')
+print(df.head())
+print()
+
+print(df.shape)
+print()
+
+df = df.drop_duplicates()
+print(df.shape)
+print()
+
+print(df.describe())
+print()
+
+print(df.info())
+print()
+
 
